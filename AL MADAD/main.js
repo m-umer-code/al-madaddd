c717588df10533b8f1429baf62fb3015a6830988
m-umer-code <umerald721@gmail.com>
2025-07-03 14:53:22 +0500

    Add files via upload

---
+++ AL MADAD/main.js
@@ -0,0 +1,8 @@
+ fruits = ["apple","banana","orange","mango","berry"]
+newarray = fruits.slice(3,5)
+ console.log(newarray)
+ console.log(fruits)
+set1 = [1,2,3]
+set2 = [4,5,6]
+let newset = set1.concat(set2)
+console.log(newset)
